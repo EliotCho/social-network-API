39c3b4e79ea50d495fd442197af02c34f2a06388
Eliot Cho <eliotcho99@gmail.com>
2024-09-02 23:09:00 -0400

    rough files, still trying to figure out what I want to do

---
+++ models/thought.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const reactionSchema = require('./reaction');
+const dateFormat = require("../utils/dateFormat");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports =  Thought;
